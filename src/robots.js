4d41d8d6d936df56c26455334a67858f704780f7
Brittany Miller <brittanydmiller@gmail.com>
2019-05-10 13:13:50 -0700

    Adding first draft

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sherry Bop',
+    username: 'pompeii',
+    email: 'sb@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Jimmy Matt',
+    username: 'mondobigdude',
+    email: 'asdf@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Nanny Chum',
+    username: 'emily',
+    email: 'fdsfa@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Penny Shuh',
+    username: '2kool',
+    email: 'asssd@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Filly Bee',
+    username: 'neveryoumind',
+    email: 'wererabbit@gmail.com'
+  }
+]
